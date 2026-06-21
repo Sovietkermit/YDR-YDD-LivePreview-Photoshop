65ea920cfb7946ad03e19b0fc493e5532f9a7562
Sovietkermit <140968591+Sovietkermit@users.noreply.github.com>
2026-06-21 16:50:37 +0200

    Add files via upload

---
+++ bridge/server_local_debug.bat
@@ -0,0 +1,5 @@
+D:
+cd D:\ydr-tool\bridge\publish
+YdrBridge.exe --watch "D:\ydrpreview_exchange" "D:\Application\CodeWalker30_dev48"
+cd D:\Application\Adobe APP\Adobe Photoshop 2025
+Photoshop.exe
